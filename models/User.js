02a106d9a620cf7b8798b6b6e4584d576a5cacfb
Leesungsup <tmdtjq0116@naver.com>
2021-12-15 16:04:09 +0900

    auth

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose=require('mongoose')
+const bcrypt=require('bcrypt')
+const saltRounds=10
+const jwt=require('jsonwebtoken')
 const userSchema=mongoose.Schema({
     name:{
         type:String,
@@ -28,11 +31,40 @@ const userSchema=mongoose.Schema({
     tokenExp:{
         type:Number
     }
-},{
+},{    
     writeConcern: {
-       w: 'majority',
-       j: true,
-       wtimeout: 1000
-    }})
+    w: 'majority',
+    j: true,
+    wtimeout: 1000
+}})
+userSchema.pre('save',function(next){
+    var user=this
+    if(user.isModified('password')){
+    bcrypt.genSalt(saltRounds,function(err,salt){
+        if(err) return next(err)
+        bcrypt.hash(user.password,salt,function(err,hash){
+            if(err) return next(err)
+            user.password=hash
+            next()
+        })
+    })
+    }else{
+        next()
+    }
+})
+userSchema.methods.comparePassword=function(plainPassword,callback){
+    bcrypt.compare(plainPassword,this.password,function(err,isMatch){
+        if(err) return callback(err);callback(null,isMatch)
+    })
+}
+userSchema.methods.generateToken=function(callback){
+    var user=this
+    var token=jwt.sign(user._id.toHexString(),'leeToken')
+    user.token=token
+    user.save(function(err,user){
+        if(err) return callback(err)
+        callback(null,user)
+    })
+}
 const User=mongoose.model('User',userSchema)
 module.exports={User}
