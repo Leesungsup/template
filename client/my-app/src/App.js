e10f1073f4646e393b66598dc0265123e8bbcd8c
Leesungsup <tmdtjq0116@naver.com>
2021-12-18 03:14:32 +0900

    component? element?

---
+++ client/my-app/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import NavBar from './components/views/NavBar/NavBar';
+import RegisterPage from './components/views/ResgisterPage/RegisterPage';
+function App() {
+  return (
+    <Router>
+      <div>
+
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Routes>
+          <Route exact path="/" element={<LandingPage />}>
+          </Route>
+          <Route exact path="/login" element={<LoginPage />}>
+          </Route>
+          <Route exact path="/register" element={<RegisterPage />}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
